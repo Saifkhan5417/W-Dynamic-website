31be518e023f479b974c1be36fc7bb1866bf0763
Saif <saiftech.it123@gamil.com>
2022-04-23 13:45:13 +0530

    This is a Dynamic Weather app by Nodejs

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const path = require("path")
+const hbs = require("hbs")
+const app  = express();
+const port = 8000;
+
+const partial_path = path.join(__dirname , "Templates/partials")
+
+hbs.registerPartials(partial_path)
+
+const static_path = path.join(__dirname , "public")
+app.use(express.static(static_path))
+
+
+const temp_path = path.join(__dirname,"Templates/views")
+
+app.set("views" , temp_path)
+app.set("view engine" , "hbs")
+
+app.get("/" , (req , res)=>{
+    res.render("index")
+})
+app.get("/about" , (req , res)=>{
+    res.render("about")
+})
+app.get("/weather" , (req , res)=>{
+    res.render("weather")
+})
+app.get("*" , (req , res)=>{
+    res.send("Something went worng ")
+})
+
+app.listen(port , ()=>{
+    console.log(`Your app is listen at ${port}`)
+})
---
+++ public/js/weaher.js
@@ -0,0 +1,91 @@
+// const async = require("hbs/lib/async");
+
+const time = document.getElementById("time");
+const day = document.getElementById("day");
+const temp = document.getElementById("temp");
+const btn = document.getElementById("btn");
+const city = document.getElementById("city");
+const search = document.getElementById("search");
+const country = document.getElementById("country");
+const mood = document.getElementById("mood");
+
+const getDateTime = () => {
+  let getTime = new Date();
+  let newDate = getTime.getDate();
+  let newTime = getTime.getHours();
+  let newMin = getTime.getMinutes();
+  if (newMin < 10) {
+    time.innerHTML = newTime + ":" + 0 + newMin;
+  } else {
+    time.innerHTML = newTime + ":" + newMin;
+  }
+
+  let newDay = getTime.getDay();
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednasday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let newMon = getTime.getMonth();
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let newYear = getTime.getFullYear();
+  day.innerHTML =
+    days[newDay] +
+    "    " +
+    "    " +
+    "    ," +
+    newDate +
+    " " +
+    months[newMon] +
+    " " +
+    newYear;
+
+  return newTime, newMin, newYear, days[newDay], months[newMon], newDate;
+};
+getDateTime();
+
+// Api work
+
+const getInfo = async (event) => {
+  event.preventDefault();
+  getsearch = search.value;
+  try {
+    let url = `https://api.weatherapi.com/v1/current.json?key=3f663890df054cefa8954759220604&q=${getsearch}&aqi=yes`;
+    const getApi = await fetch(url);
+    const jsonData = await getApi.json();
+    const arrData = [jsonData];
+    temp.innerHTML = arrData[0].current.temp_c + "°C";
+    // mood of weather
+    mood.innerHTML = arrData[0].current.condition.text;
+    // set city and country name
+    let s_city = arrData[0].location.name;
+    city.innerHTML = s_city;
+    let s_country = arrData[0].location.country;
+    console.log(s_country);
+    country.innerHTML = s_country;
+  } catch (error) {
+    console.log(error);
+  }
+
+  //   alert(getsearch);
+};
+
+btn.addEventListener("click", getInfo);
+getsearch = " ";
