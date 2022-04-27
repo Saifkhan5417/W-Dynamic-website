502dd06a1ab4aa89e9745c81c4a1e0f2f616d868
Saif <saiftech.it123@gamil.com>
2022-04-27 13:10:41 +0530

    adding some varification

--- public/js/weaher.js
+++ public/js/weaher.js
@@ -14,12 +14,14 @@ const getDateTime = () => {
   let newDate = getTime.getDate();
   let newTime = getTime.getHours();
   let newMin = getTime.getMinutes();
+  let newsec = getTime.getSeconds();
+    console.log(setTimeout( newsec, 1000));
   if (newMin < 10) {
-    time.innerHTML = newTime + ":" + 0 + newMin;
+    time.innerHTML = newTime + ":" + 0 + newMin+":"+newsec;
   } else {
-    time.innerHTML = newTime + ":" + newMin;
+    time.innerHTML = newTime + ":" + newMin+":"+newsec;
   }
-
+  setTimeout( getDateTime, 1000)
   let newDay = getTime.getDay();
   const days = [
     "Sunday",
@@ -57,7 +59,7 @@ const getDateTime = () => {
     " " +
     newYear;
 
-  return newTime, newMin, newYear, days[newDay], months[newMon], newDate;
+  return newTime, newMin, newYear, days[newDay], months[newMon], newDate,newsec;
 };
 getDateTime();
 
@@ -66,7 +68,13 @@ getDateTime();
 const getInfo = async (event) => {
   event.preventDefault();
   getsearch = search.value;
+  
+  if (getsearch=="") {
+    alert("Enter Any Location")
+    console.log("Enter Any Location")
+  }else{
   try {
+    
     let url = `https://api.weatherapi.com/v1/current.json?key=3f663890df054cefa8954759220604&q=${getsearch}&aqi=yes`;
     const getApi = await fetch(url);
     const jsonData = await getApi.json();
@@ -80,10 +88,11 @@ const getInfo = async (event) => {
     let s_country = arrData[0].location.country;
     console.log(s_country);
     country.innerHTML = s_country;
+  
   } catch (error) {
     console.log(error);
-  }
-
+  }}
+  
   //   alert(getsearch);
 };
 
